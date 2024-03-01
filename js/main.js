e0b41ce23ed82bceb6369d048ff65fae3d62b226
MaximeWEBER <129857968+MaximeWEBER@users.noreply.github.com>
2024-03-01 16:43:10 +0100

    Test d'ajout ./

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    links.forEach(link => {
-      link.classList.toggle('fade');
-    });
-    hamburger.classList.toggle('toggle');
-});
-  
-
+const hamburger=document.querySelector(".hamburger"),navLinks=document.querySelector(".nav-links"),links=document.querySelectorAll(".nav-links li");hamburger.addEventListener("click",(()=>{navLinks.classList.toggle("open"),links.forEach((e=>{e.classList.toggle("fade")})),hamburger.classList.toggle("toggle")}));
